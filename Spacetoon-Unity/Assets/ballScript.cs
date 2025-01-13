4852ca10eca2beb04d43982ef9dbad8734e9fa68
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr>
2025-01-13 15:02:20 +0100

    Resolving problems

--- Spacetoon-Unity/Assets/ballScript.cs
+++ Spacetoon-Unity/Assets/ballScript.cs
@@ -26,14 +26,29 @@ public class ballScript : MonoBehaviour
         if(other.tag == "red")
          {
             blueTxt.text = (++blueScore).ToString();
+            AudioManager.instance.PlayEffect(AudioManager.instance.goalSound); // Son de but
         }
         else if(other.tag == "blue")
          {
             redTxt.text = (++redScore).ToString();
+            AudioManager.instance.PlayEffect(AudioManager.instance.goalSound); // Son de but
         }
         control.respawnBall();
         red.respawn();
         blue.respawn();
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    if (collision.collider.CompareTag("Player"))
+    {
+        AudioManager.instance.PlayEffect(AudioManager.instance.blopSound); // Son de collision
+    }
+    else
+    {
+        AudioManager.instance.PlayEffect(AudioManager.instance.respawnSound); // Son pour toucher le bord
+    }
+}
    
 }
